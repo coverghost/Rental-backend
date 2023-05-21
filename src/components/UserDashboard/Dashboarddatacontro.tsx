c5077acb0f53f48413afcc0f2cb3c690cb1c80eb
Ayush arya <ayush@brightdigitalgold.com>
2023-05-21 17:12:43 +0530

    token for user login

--- src/components/UserDashboard/Dashboarddatacontro.tsx
+++ src/components/UserDashboard/Dashboarddatacontro.tsx
@@ -9,19 +9,15 @@ import jwt from "jsonwebtoken";
 export const GetUserInfo = async (request: Request, response: Response) => {
     console.log("from backens userinfo--->>>>>>>>", request.body);
     const token = request.body.token;
-    // const token = request.headers.authorization?.split(" ")[1] as string; // Add type assertion to specify token as string
     console.log("token---------------- line 81 ------->>>", token);
     try {
       const decodedToken: any = jwt.verify(token, "your-secret-key");
       const mobile = decodedToken.mobile;
-      const userId = decodedToken.userId
-      
-      // Query the MongoDB database using the mobile number to retrieve user information
       const user = await User.findOne({ "personal.mobile": mobile });
   
       if (user) {
-        // Include the user information in the response
-        return response.json({ success: true, userId: userId });
+        return response.json({ success: true, user:user });
+        
       }
   
       return response.status(404).json({
